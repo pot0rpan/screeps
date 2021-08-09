49a7edb8531b390576f078e7488de0dc9e6ed9f0
pot <57581179+pot0rpan@users.noreply.github.com>
2021-08-08 21:36:00 -0600

    some console color

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,7 @@ global.isFirstTick = true;
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+  console.log(`<span style="color:#44ff88">-- Tick ${Game.time} --</span>`);
 
   // Delete memory of missing creeps
   if (isNthTick(config.ticks.DELETE_DEAD_CREEP_MEMORY)) {
