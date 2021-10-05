b2383a9387de942b75445866337373e80032338c
pot <57581179+pot0rpan@users.noreply.github.com>
2021-10-05 15:49:26 -0600

    better tower damage calculation

--- src/utils/combat.ts
+++ src/utils/combat.ts
@@ -6,14 +6,36 @@ export function getMaxHeal(creeps: Creep[]): number {
   );
 }
 
+// https://github.com/Arcath/screeps-fns/blob/master/src/tower-effectiveness-at-range/index.ts
+/**
+ * Calculate the effectiveness of a tower at the given range
+ *
+ * @param range The range to calculate effectiveness at e.g. `tower.pos.getRangeTo(target)`
+ * @param max The power of the tower, e.g. `TOWER_POWER_ATTACK`
+ */
+function towerEffectivenessAtRange(range: number, max: number): number {
+  if (range <= TOWER_OPTIMAL_RANGE) {
+    return max;
+  }
+  if (range >= TOWER_FALLOFF_RANGE) {
+    return max * (1 - TOWER_FALLOFF);
+  }
+
+  const towerFalloffPerTile =
+    TOWER_FALLOFF / (TOWER_FALLOFF_RANGE - TOWER_OPTIMAL_RANGE);
+
+  return max * (1 - (range - TOWER_OPTIMAL_RANGE) * towerFalloffPerTile);
+}
+
 export function getMaxTowerDamage(towers: StructureTower[], pos: RoomPosition) {
   return towers
     .filter(tower => tower.store.getUsedCapacity(RESOURCE_ENERGY) > 10)
-    .reduce((dmg, tower) => {
-      const range = pos.getRangeTo(tower);
-      if (range <= 5) return dmg + 600;
-      if (range >= 20) return dmg + 150;
-      // TODO: Calculate actual damage
-      return dmg + 300;
-    }, 0);
+    .reduce(
+      (dmg, tower) =>
+        (dmg += towerEffectivenessAtRange(
+          pos.getRangeTo(tower),
+          TOWER_POWER_ATTACK
+        )),
+      0
+    );
 }
